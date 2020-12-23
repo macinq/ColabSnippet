aceee46fde7be64b0d17c187ab127d6373113a12
Sergey Igoshin <macinq@gmail.com>
2020-12-23 19:54:22 +0400

    Delete ImStillHere.js

--- ImStillHere.js
+++
@@ -1,8 +0,0 @@
-function ClickConnect(){
-console.log("Working"); 
-document
-  .querySelector('#top-toolbar > colab-connect-button')
-  .shadowRoot.querySelector('#connect')
-  .click() 
-}
-setInterval(ClickConnect,300000)
